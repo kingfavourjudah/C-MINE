d2f949df0b70cc0d7651b330b5f08076ec55cdb0
Lucky Nweke <75287069+LuckyNwekeKFC@users.noreply.github.com>
2021-12-07 10:43:54 +0100

    Update C-MINE.sol

--- C-MINE.sol
+++ C-MINE.sol
@@ -1,55 +1,12 @@
-//SPDX-License-Identifier: Unlicensed
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-pragma solidity >0.6.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
-
-// Example class - a mock class using delivering from ERC20
-contract C-MINE is ERC20 {
-
-    uint256 public _totalSupply;
-    mapping (address => uint256) private _balances;
-    // uint256 public _maxWalletToken;
-    
-    // uint256 team = 3; //Team: 3%
-    // uint256 _burnable = 2; //Burn: 2%
-    // uint256 maxWallet = 2; //max wallet holding limited at 2%
-    
-    uint256 reflectionFee = 2; //2% for every buy and sell transaction
-    uint256 marketingFee = 3; //Marketing: 3%
-    uint256 totalFee = 5; //total Fee to charge
-    uint256 _denominator = 100;
-    
-    mapping(address => int64) dumpCounter; // a counter for the number of sell order before a user can sell all their coins
-    
-    constructor() ERC20("C-MINE", "C-MINE") public {
-        _totalSupply = 747609657000000000000000000;
-        _mint(msg.sender, _totalSupply);
-    }
-
-    function transfer(address to, uint tokens) public virtual override returns (bool) {
-        require(tokens <= balanceOf(msg.sender),"Insufficient Balance.");
-
-        //place a sell order after 3 sells
-        if(balanceOf(msg.sender) == tokens){
-            dumpCounter[msg.sender] += 1; //increment counter for sell
-            require(dumpCounter[msg.sender] == 3, "Transaction error");
-        }
-        
-        dumpCounter[msg.sender] = 0; //reset sell count for seller
-
-        uint256 amountReceived = takeFee(msg.sender,tokens);
-        
-        _transfer(msg.sender, to, amountReceived);
-        return true;
-    }
-
-    function takeFee(address sender, uint256 amount) public virtual returns (uint256) {
-        uint256 feeAmount = amount * (totalFee / _denominator);
-
-        _transfer(sender, address(this), feeAmount);
-
-        return amount - feeAmount;
+/// @custom:security-contact lucky@crypto528.com
+contract CMINE is ERC20, ERC20Burnable {
+    constructor() ERC20("C-MINE", "C-MINE") {
+        _mint(msg.sender, 747609657 * 10 ** 18());
     }
 }
-
